7d99e9f3ae027bb9f37918a3743345b96e816ca3
Krzysztof Rakowski <krzysztof@rakowski.pl>
2016-03-09 16:45:31 +0100

    added oneline calculation, argument parsing and other cosmetic changes

--- b2n.py
+++ b2n.py
@@ -3,20 +3,28 @@ from bs4 import BeautifulSoup
 import pycurl
 from StringIO import StringIO
 from urllib import urlencode
+from datetime import date
+import argparse
+
+parser = argparse.ArgumentParser(description="Calculate net salary from gross salary.")
+parser.add_argument('amount', type=int, help="Gross amount per month.")
+parser.add_argument('--oneline', dest="oneline", default=False, action='store_true', help="Return average value as oneline response.")
+
+args = parser.parse_args()
 
 buf = StringIO()
 c = pycurl.Curl()
 c.setopt(c.URL, 'http://www.infor.pl/kalkulatory/kalkulator-wynagrodzen-plac.html')
 
-kwota = 10000
+amount = args.amount
 
 post_data = {
-    'kwota': kwota,
-    'rok':	2016
+    'kwota': amount,
+    'rok':	date.today().year
 }
 
 for i in range(1,13):
-    post_data['miesiac[%s]' % i] = kwota
+    post_data['miesiac[%s]' % i] = amount
 
 postfields = urlencode(post_data)
 c.setopt(c.POSTFIELDS, postfields)
@@ -48,21 +56,24 @@ netto = (
 
 srednio = sum(netto)/12
 
-print """
-Wynagrodzenie netto:
+if args.oneline:
+    print "%.0f" % srednio
+else:
+    print """
+    Wynagrodzenie netto:
 
-styczen     %.0f PLN
-luty        %.0f PLN
-marzec      %.0f PLN
-kwiecien    %.0f PLN
-maj         %.0f PLN
-czerwiec    %.0f PLN
-lipiec      %.0f PLN
-sierpien    %.0f PLN
-wrzesien    %.0f PLN
-pazdziernik %.0f PLN
-listopad    %.0f PLN
-grudzien    %.0f PLN
+    styczen     %.0f PLN
+    luty        %.0f PLN
+    marzec      %.0f PLN
+    kwiecien    %.0f PLN
+    maj         %.0f PLN
+    czerwiec    %.0f PLN
+    lipiec      %.0f PLN
+    sierpien    %.0f PLN
+    wrzesien    %.0f PLN
+    pazdziernik %.0f PLN
+    listopad    %.0f PLN
+    grudzien    %.0f PLN
 
-srednio:    %.0f PLN miesiecznie
-""" % (netto+(srednio,))
+    srednio:    %.0f PLN miesiecznie
+    """ % (netto+(srednio,))
