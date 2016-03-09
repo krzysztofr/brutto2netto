355303267a78dc1e01f3511205b08f990f7e40b0
Krzysztof Rakowski <krzysztof@rakowski.pl>
2016-03-09 16:30:48 +0100

    calculation of average and per/month added

--- b2n.py
+++ b2n.py
@@ -8,7 +8,7 @@ buf = StringIO()
 c = pycurl.Curl()
 c.setopt(c.URL, 'http://www.infor.pl/kalkulatory/kalkulator-wynagrodzen-plac.html')
 
-kwota = 7800
+kwota = 10000
 
 post_data = {
     'kwota': kwota,
@@ -31,19 +31,38 @@ body = buf.getvalue().decode('utf8')
 
 bs = BeautifulSoup(body, 'html.parser')
 
-netto = {
-        1: bs.find_all('td')[6].string,
-        2: bs.find_all('td')[15].string,
-        3: bs.find_all('td')[24].string,
-        4: bs.find_all('td')[33].string,
-        5: bs.find_all('td')[42].string,
-        6: bs.find_all('td')[51].string,
-        7: bs.find_all('td')[60].string,
-        8: bs.find_all('td')[69].string,
-        9: bs.find_all('td')[78].string,
-        10: bs.find_all('td')[87].string,
-        11: bs.find_all('td')[96].string,
-        12: bs.find_all('td')[105].string
-    }
+netto = (
+        float(bs.find_all('td')[6].string),
+        float(bs.find_all('td')[15].string),
+        float(bs.find_all('td')[24].string),
+        float(bs.find_all('td')[33].string),
+        float(bs.find_all('td')[42].string),
+        float(bs.find_all('td')[51].string),
+        float(bs.find_all('td')[60].string),
+        float(bs.find_all('td')[69].string),
+        float(bs.find_all('td')[78].string),
+        float(bs.find_all('td')[87].string),
+        float(bs.find_all('td')[96].string),
+        float(bs.find_all('td')[105].string)
+)
 
-print netto
+srednio = sum(netto)/12
+
+print """
+Wynagrodzenie netto:
+
+styczen     %.0f PLN
+luty        %.0f PLN
+marzec      %.0f PLN
+kwiecien    %.0f PLN
+maj         %.0f PLN
+czerwiec    %.0f PLN
+lipiec      %.0f PLN
+sierpien    %.0f PLN
+wrzesien    %.0f PLN
+pazdziernik %.0f PLN
+listopad    %.0f PLN
+grudzien    %.0f PLN
+
+srednio:    %.0f PLN miesiecznie
+""" % (netto+(srednio,))
